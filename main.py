0b836bcb0a28dd9b2e2acf845be80d367f948915
Tam <enquiretam@gmail.com>
2024-09-20 10:30:32 +0100

    added in food and scoreboard features

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,16 +11,25 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.5)
-    
+    time.sleep(0.1)
     snake.move()
     
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+        
 
 screen.exitonclick()
 
